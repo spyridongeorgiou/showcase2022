7d2268a582894e1acc63d38005917eb36aef96e4
spyridongeorgiou <71075658+spyridongeorgiou@users.noreply.github.com>
2022-12-14 12:18:26 +0100

    Changed and added links

---
+++ Lesehilfe/psshortcut2.ps1
@@ -0,0 +1,10 @@
+﻿# Create a Shortcut with Windows PowerShell
+$SourceFileLocation = "C:\EDV\Lesehilfe\Lesehilfe.exe"
+$ShortcutLocation = "C:\Users\Public\Desktop\Lesehilfe.lnk"
+#New-Object : Creates an instance of a Microsoft .NET Framework or COM object.
+#-ComObject WScript.Shell: This creates an instance of the COM object that represents the WScript.Shell for invoke CreateShortCut
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+$Shortcut.TargetPath = $SourceFileLocation
+#Save the Shortcut to the TargetPath
+$Shortcut.Save()
